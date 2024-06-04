3e9a2c8707157924212803da37299c1cd162b4a2
Tejas Tejam <vasudevtejam@outlook.com>
2024-06-04 20:12:49 +0530

    Backend : Remove request limit

--- backend/app.js
+++ backend/app.js
@@ -33,7 +33,7 @@ app.use(
 app.use(express.static(path.join(__dirname, "public")));
 
 // Body parser , reading data from body into req.body
-app.use(express.json({ limit: "10kb" }));
+app.use(express.json());
 
 app.use(morgan("dev"));
 
